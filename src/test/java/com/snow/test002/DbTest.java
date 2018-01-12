790d0b8988223a947b05cb5368257894dfdd9c1d
iv0100952 <iv0100952@BRKim.corp.doosan.com>
2018-01-12 18:04:25 +0900

    접속 테스트 추가

---
+++ src/test/java/com/snow/test002/DbTest.java
@@ -0,0 +1,23 @@
+package com.snow.test002;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+public class DbTest {
+	
+	@Test
+	public void dbTest() throws Exception {
+	    Class.forName("com.mysql.jdbc.Driver");
+	    
+	    try {
+		    Connection con =  DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/myDb","root","qwe123");
+		    assertThat(con, is(notNullValue()));
+	    } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+}
